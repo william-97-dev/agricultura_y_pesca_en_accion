be941fe2010c40f49727cd8e13f4b440dc4c72d4
william-444 <williamalvarenga043@gmail.com>
2026-02-27 00:21:55 -0600

    Hero version laptop completo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,15 +7,15 @@ const Hero = () => {
             style= {{ backgroundImage: `url(${imagenHero}` }}
         >
             <div className='absolute inset-0 bg-black/60 z-0'></div>
-            <header className='max-w-xl p-4 text-white z-5'>
-                <h1 className='text-5xl font-bold mb-8'>
+            <header className='max-w-xl p-2 text-white z-5 lg:max-w-4xl lg:mr-96 lg:p-2'>
+                <h1 className='text-3xl font-bold mb-6 lg:text-6xl lg:mb-8 leading-tight'>
                     Redes hechas por manos expertas, resultados de confianza.
                 </h1>
-                <p className='text-white/90'>
-                    Con más de 30 años de experiencia, fabricamos atarrayas resistentes
+                <p className='text-white/90 lg:text-lg max-w-2xl'>
+                    Con más de 20 años de experiencia, fabricamos atarrayas resistentes
                     y trasmallos de calidad superior para una pesca confiable.
                 </p>
-                <button className='bg-(--primary-yellow) text-(--primary-blue) font-bold cursor-pointer border-none px-6 py-4 rounded-md mt-4'>
+                <button className='bg-(--primary-yellow) text-(--primary-blue) font-bold cursor-pointer border-none px-6 py-4 rounded-md mt-4 lg:mt-6 hover:bg-(--primary-blue) hover:text-(--primary-yellow) transition-colors duration-700 ease-in-out'>
                     Ver catálogo
                 </button>
             </header>
