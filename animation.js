42e4cd9381ac19f3702154c6178cd98b7b633aad
william-444 <williamalvarenga043@gmail.com>
2026-03-03 23:33:43 -0600

    animacion con gsap, nav y titulo del hero

--- animation.js
+++ animation.js
@@ -1,11 +1,53 @@
 import gsap from "gsap";
 
+export const animationLogo = (logo) => {
+    gsap.fromTo(logo, {
+        x: -100,
+        rotate: 360,
+        scale: 0
+    },
+        {
+            x: 0,
+            rotate: 0,
+            scale: 1,
+            duration: 2,
+            ease: "bounce.out"
+        })
+}
+
+export const animationSocialMedia = (socialMedia) => {
+    gsap.fromTo(socialMedia, {
+        //estado inicial
+        x: -100,
+        scale: 0
+    },// estado final
+        {
+            x: 0,
+            scale: 1,
+            duration: 1,
+            ease: "sine.inOut"
+        })
+}
+
 export const animationTitle = (h1Principal) => {
     gsap.fromTo(h1Principal, {
         y: -100,
         opacity: 0,
-        //stagger: 0.25
-        //podemos usar stagger si hay varios elementos para entrar uno por uno ejemplo en un nav
+    },//Estado inicial
+        {
+            y: -0,
+            opacity: 1,
+            ease: "power3.out",
+            duration: 1.5
+        }//Estado final
+    )
+}
+
+export const animationNav = (listNav) => {
+    gsap.fromTo(listNav, {
+        y: -100,
+        opacity: 0,
+        stagger: 1 //podemos usar stagger si hay varios elementos para entrar uno por uno
     },//Estado inicial
         {
             y: -0,
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import miLogo from '../assets/images/Logo_nav.png'
 import menu from '../assets/icons/menu.svg'
 import { FaFacebook, FaTiktok, FaWhatsapp, FaYoutube } from "react-icons/fa"
+import { animationNav } from '../../animation'
+import { animationLogo } from '../../animation'
+import { animationSocialMedia } from '../../animation'
 
 const Navbar = () => {
     const [open, setOpen] = useState(false)
+    const boxRef = useRef(null)
+    const boxLog = useRef(null)
+    const boxSocialMedia = useRef(null)
 
     const handleScroll = (id) => {
         const section = document.getElementById(id)
@@ -12,21 +18,27 @@ const Navbar = () => {
         setOpen(false)
     }
 
+    useEffect(() => {
+        animationNav(boxRef.current)
+        animationLogo(boxLog.current)
+        animationSocialMedia(boxSocialMedia.current)
+    }, [])
+
     return (
         <>
             <nav className='fixed flex items-center top-0 left-0 w-full z-50 h-14 lg:h-20 lg:max-w-6xl lg:left-1/2 lg:-translate-x-1/2 lg:top-4'>
                 <div className='w-full mx-auto flex justify-between items-center px-4 lg:h-20 lg:justify-between'>
-                    <div className='bg-gray-300 rounded-full h-10 w-10 lg:h-16 lg:w-16 flex items-center justify-center lg:p-2'>
+                    <div ref={boxLog} className='bg-gray-300 rounded-full h-10 w-10 lg:h-16 lg:w-16 flex items-center justify-center lg:p-2'>
                         <img src={miLogo} alt="Logo de Agricultura y Pesca" className='h-5 lg:h-7' />
                     </div>
                     {/**MENU DESKTOP */}
-                    <div className='hidden lg:flex lg:items-center gap-8 font-medium lg:text-2xl text-[#6acfc7]'>
+                    <div ref={boxRef} className='hidden lg:flex lg:items-center gap-8 font-medium lg:text-2xl text-[#6acfc7]'>
                         <button onClick={() => handleScroll("inicio")} className='cursor-pointer'>Inicio</button>
                         <button onClick={() => handleScroll("catalogo")} className='cursor-pointer'>Catálogo</button>
                         <button onClick={() => handleScroll("envios")} className='cursor-pointer'>Envíos</button>
                     </div>
                     {/**REDES SOCIALES DESKTOP */}
-                    <div className='hidden lg:flex lg:gap-8 lg:text-3xl'>
+                    <div ref={boxSocialMedia} className='hidden lg:flex lg:gap-8 lg:text-3xl'>
                         <a
                             href="https://www.facebook.com/share/1CF9CB7cJa/"
                             target='_blank'
